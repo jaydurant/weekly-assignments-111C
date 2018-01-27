17152194104443168453ba83e4fb56513a5ca966
jason durant <durantjay1@gmail.com>
2018-01-27 15:51:13 -0800

    Make changes to employee class to support new input method for changing employee values

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class Employee {
 	 private String name;
 	 private String DOJ;
@@ -14,11 +14,17 @@ public class Employee {
 		 this.id = id;
 	 }
 	 
-	 public void input(String name, String DOJ, String dept,int id) {
-		 this.name = name;
-		 this.DOJ = DOJ;
-		 this.dept = dept;
-		 this.id = id;
+	 public void input() {
+		 Scanner input = new Scanner(System.in);
+		 System.out.println("Enter Name");
+		 this.name = input.nextLine();
+		 System.out.println("Enter DOJ");
+		 this.DOJ = input.nextLine();
+		 System.out.println("Enter Dept");
+		 this.dept = input.nextLine();
+		 System.out.println("Enter Id");
+		 this.id = input.nextInt();
+		 
 	 }
 	 
 	 public String display() {
--- src/runEmployee.java
+++ src/runEmployee.java
@@ -4,6 +4,9 @@ public class runEmployee {
 	public static void main(String[] args) {
 		Employee emp = new Employee("Jason","test","science",1);
 		System.out.println(emp.display());
+		emp.input();
+		
+		System.out.println(emp.display());
 	}
 
 }
